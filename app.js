2aef4189fc54f9d1350f7d845864da19218d2a0b
Querido, Abraham <Abraham.Querido@ellucian.com>
2023-05-24 12:52:44 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+
+const port = process.env.PORT || 3000;
+
+// connect to db
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
